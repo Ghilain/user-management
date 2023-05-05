865c98a5e57117c7feaaf54852e3880c07a8aaf9
Ghilain <ishimweghilain5050@gmail.com>
2023-05-05 16:31:32 +0200

    add miidleware

--- server/index.js
+++ server/index.js
@@ -1,3 +1,14 @@
-const express =require("express");
+const express = require("express");
 const app = express();
 const cors = require("cors");
+
+//Middleware
+
+app.use(express.json()); // help for req.boy
+app.use(cors());
+
+//Routes//
+
+app.listen(5000, () => {
+    console.log("server running on port 5000");
+});
